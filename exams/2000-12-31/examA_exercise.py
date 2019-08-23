9f0514f5a01117656fe537639a758356a6c66e05
biancol <luca.bianco@fmach.it>
2019-08-23 11:02:44 +0200

    first commit

---
+++ exams/2000-12-31/examA_exercise.py
@@ -0,0 +1,15 @@
+
+
+class MyClass:
+
+    """ 
+        This class does... stuff
+    """
+    def do_something(self):
+        raise Exception('TODO IMPLEMENT ME !')
+
+ 
+    def do_something_else(self):
+        raise Exception('TODO IMPLEMENT ME !')
+        
+
---
+++ toc.rst
@@ -0,0 +1,20 @@
+
+
+.. toctree::
+    :caption: Index
+    :maxdepth: 2
+    :glob:
+
+
+    Home <index.ipynb>
+    
+
+|space|
+==========
+
+.. |space| unicode:: U+0020 .. space
+
+
+
+Index
+-----------
